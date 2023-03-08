95d64af6506d1f5c56b822b5608ddda9b0638903
Alexander Obregon <alexander.obregon47@gmail.com>
2023-03-08 14:56:28 -0800

    added while loop and length++ logic

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -2,7 +2,12 @@ class Solution {
     public int lengthOfLastWord(String s) {
         int p = s.length();
         int length= 0;
-        
+        while(p > 0){
+            p--;
+            if( s.charAt(p) != ' ') {
+                length++;
+            }
+        }
         return 1;
     }
 }
