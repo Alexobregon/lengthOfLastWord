b5db9da32fd97629b6d10ed9453838250c32fe3b
Alexander Obregon <alexander.obregon47@gmail.com>
2023-03-12 19:01:47 -0700

    added loop comment

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -2,7 +2,7 @@ class Solution {
     public int lengthOfLastWord(String s) {
         int p = s.length(); // initialize pointer at end of string
         int length= 0; // initialize word length to 0
-        while(p > 0){
+        while(p > 0){ // loop through string backwards
             p--;
             if( s.charAt(p) != ' ') {
                 length++;
