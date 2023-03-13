2a0b9e881ac3811b01fee218af87d543fa544eba
Alexander Obregon <alexander.obregon47@gmail.com>
2023-03-12 19:02:31 -0700

    added if statement comment

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -4,7 +4,7 @@ class Solution {
         int length= 0; // initialize word length to 0
         while(p > 0){ // loop through string backwards
             p--;
-            if( s.charAt(p) != ' ') {
+            if( s.charAt(p) != ' ') { // if the character is not a space
                 length++;
             } else if (length > 0) {
                 return length;
